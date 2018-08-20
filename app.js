ed5d77463ae1e074e61c4ec8ecde4c8cf4ad81cb
cyr666 <792849953@qq.com>
2018-08-20 18:20:19 +0800

    job

---
+++ app.js
@@ -0,0 +1,76 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  editTabBar: function () {
+    var tabbar = this.globalData.tabbar,
+      currentPages = getCurrentPages(),
+      _this = currentPages[currentPages.length - 1],
+      pagePath = _this.__route__;
+    (pagePath.indexOf('/') != 0) && (pagePath = '/' + pagePath);
+    for (var i in tabbar.list) {
+      tabbar.list[i].selected = false;
+      (tabbar.list[i].pagePath == pagePath) && (tabbar.list[i].selected = true);
+    }
+    _this.setData({
+      tabbar: tabbar
+    });
+    // console.log(_this.data.tabbar)
+  },
+  globalData: {
+    userInfo: null,
+    serverUrl: 'https://api.wilog.cn/',
+    tabbar: {
+      color: "#9A9A9A",
+      selectedColor: "#008EFF",
+      backgroundColor: "#FAFAFA",
+      borderStyle: "",
+      isIphoneX: false,
+      list: [
+        {
+          pagePath: "/pages/index/index",
+          text: "首页",
+          selected: true,
+          
+        },
+        {
+          pagePath: "/pages/personal/personal",
+          text: "我的",
+          selected: false
+        },
+      ],
+      position: "bottom"
+    },
+  }
+})
